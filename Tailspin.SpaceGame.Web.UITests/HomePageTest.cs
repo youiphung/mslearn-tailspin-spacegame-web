188f657ee78e81cfe10db4e6677d5455725bb321
Youi Phung <yphung@rdt.co.uk>
2023-12-13 17:14:02 +1030

    For demo and practice purpose, use Chrome for testing instead of Edge as easier to sync driver version in the hosted agent.

--- Tailspin.SpaceGame.Web.UITests/HomePageTest.cs
+++ Tailspin.SpaceGame.Web.UITests/HomePageTest.cs
@@ -9,9 +9,9 @@ using System.Collections;
 
 namespace UITests
 {
-    //[TestFixture("Chrome")]
+    [TestFixture("Chrome")]
     //[TestFixture("Firefox")]
-    [TestFixture("Edge")]
+    //[TestFixture("Edge")]
     public class HomePageTest
     {
         private string browser;
